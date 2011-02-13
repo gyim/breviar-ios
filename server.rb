68a981b4525354986a0079d30de40120ff81077f
Juraj Vidéky <videky@breviar.sk>
2011-02-13 22:17:33 +0100

    2005-04-01

--- server.rb
+++ server.rb
@@ -2,22 +2,15 @@
 require 'webrick'
     include WEBrick
 
-CGIRunner = "\"#{Ruby}\" \"#{Config::LIBDIR}/httpservlet/cgi_runner.rb\""
-
-# http://www.talkaboutprogramming.com/group/comp.lang.ruby/messages/78404.html
-
 s = HTTPServer.new(
 	:Port => 2000, 
-	:DocumentRoot => Dir::pwd, 
-	:CGIInterpreter => "\"#{WEBrick::HTTPServlet::CGIHandler::Ruby}\""
+	:DocumentRoot => Dir::pwd
 )
 
 # mount subdirectories
 
 s.mount("/include",HTTPServlet::FileHandler, Dir.pwd+"/include")
-# s.mount("/cgi-bin",HTTPServlet::FileHandler, Dir.pwd+"/cgi-bin")
-s.mount("/cgi-bin", HTTPServlet::CGIHandler, Dir.pwd+"/cgi-bin")
-s.mount("/cgi-bin/uncgi.cgi",HTTPServlet::CGIHandler, Dir.pwd+"/cgi-bin/uncgi.cgi")
+s.mount("/cgi-bin",HTTPServlet::FileHandler, Dir.pwd+"/cgi-bin")
 
 trap("INT"){ s.shutdown }
 s.start
