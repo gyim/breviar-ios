f190ad239452f75e33bbdf0e494035fde7b910d6
Juraj Vidéky <videky@breviar.sk>
2011-02-14 16:47:43 +0100

    2008-06-17

--- cgi-bin/source/dbzaltar.cpp
+++ cgi-bin/source/dbzaltar.cpp
@@ -11232,6 +11232,7 @@ label_25_MAR:
 								set_spolocna_cast(sc, poradie_svaty);
 
 							modlitba = MODL_RANNE_CHVALY;
+							_vlastna_cast_benediktus;
 							_vlastna_cast_modlitba;
 
 							modlitba = MODL_POSV_CITANIE;
@@ -11239,11 +11240,12 @@ label_25_MAR:
 							_vlastna_cast_2citanie;
 
 							modlitba = MODL_VESPERY;
+							_vlastna_cast_magnifikat;
 							_vlastna_cast_modlitba;
 
 							break;
 						}
-						/* 2006-09-12: pôvodne bola ¾ubovo¾ná spomienka na Sv. Klementa Marie Hofbauera - od 29.8.1991 památka
+						/* 2006-09-12: pôvodne bola ¾ubovo¾ná spomienka na Sv. Klementa Marie Hofbauera - od 29.8.1991 památka pre Moravu; "v èechách si lidé mohou vybrat buï Klementa nebo Bernardina na Moravì máme pøedepsaného jenom Klementa." - p. Franta, 2008-05-21 */
 						else if(poradie_svaty == 2){
 							if(query_type != PRM_DETAILY)
 								set_spolocna_cast(sc, poradie_svaty);
@@ -11260,21 +11262,22 @@ label_25_MAR:
 
 							break;
 						}
-						*/
+						/*
 						_global_svaty1.typslav = SLAV_SPOMIENKA;
-						/* _global_svaty1.typslav_lokal = LOKAL_SLAV_MORAVA_SPOMIENKA; */
 						_global_svaty1.smer = 10;
+						*/
+						_global_svaty1.typslav = SLAV_LUB_SPOMIENKA;
+						_global_svaty1.smer = 12;
+						_global_svaty1.typslav_lokal = LOKAL_SLAV_MORAVA_SPOMIENKA;
 						mystrcpy(_global_svaty1.meno, text_MAJ_20_1[_global_jazyk], MENO_SVIATKU);
 						_global_svaty1.spolcast = _encode_spol_cast(MODL_SPOL_CAST_DUCH_PAST_KNAZ);
 						_global_svaty1.farba = LIT_FARBA_BIELA; /* 2006-08-19: pridané */
-						/*
 						pocet = 2;
 						_global_svaty2.typslav = SLAV_LUB_SPOMIENKA;
 						_global_svaty2.smer = 12;
 						mystrcpy(_global_svaty2.meno, text_MAJ_20_2[_global_jazyk], MENO_SVIATKU);
 						_global_svaty2.spolcast = _encode_spol_cast(MODL_SPOL_CAST_DUCH_PAST_KNAZ, MODL_SPOL_CAST_SV_MUZ_REHOLNIK);
 						_global_svaty2.farba = LIT_FARBA_BIELA;
-						*/
 					}
 					break;
 				case 21: /* 2006-08-16: pridané */
@@ -11775,6 +11778,7 @@ label_25_MAR:
 								set_spolocna_cast(sc, poradie_svaty);
 
 							modlitba = MODL_RANNE_CHVALY;
+							_vlastna_cast_benediktus; /* 2008-06-17: doplnené */
 							_vlastna_cast_modlitba;
 
 							modlitba = MODL_POSV_CITANIE;
--- cgi-bin/source/mydefs.h
+++ cgi-bin/source/mydefs.h
@@ -59,7 +59,7 @@
 /***************************************************************/
 #include <stdio.h>
 
-#define BUILD_DATE "2008-06-10"
+#define BUILD_DATE "2008-06-17"
 
 #define BYTES 250
 #define SMALL 80
