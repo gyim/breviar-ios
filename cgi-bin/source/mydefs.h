6b577126523e967ab952dbf974e719d4f6e198f1
Juraj Vidéky (WT) <videky@breviar.sk>
2011-10-24 12:57:56 +0200

    code cleanup

--- cgi-bin/source/mydefs.h
+++ cgi-bin/source/mydefs.h
@@ -69,54 +69,49 @@
 #define SMALL 80
 #define VERY_SMALL 8
 
-/* navratova hodnota funkcii */
+// navratova hodnota funkcii
 #define NO_RESULT 2
 #define FAILURE 1
 #define SUCCESS 0
 
-/* true -- false */
+// true -- false
 #define TRUE  1
 #define FALSE 0
 #define ANO   1
 #define NIE   0
 
-#define MAX_GLOBAL_STR		2048 /* _global_string; 31/03/2000A.D.; 2010-12-07: rozöÌrenÈ; 2011-01-31: rozöÌrenÈ kvÙli text_JAN_31_SDB[]; 2011-02-02: rozöÌrenÈ kvÙli text_FEB_04_SJ[]; */
-#define MAX_GLOBAL_STR2		20 /* _global_string2, pridanÈ 2006-08-19 */
-#define MAX_GLOBAL_STR_FARBA 350 /* _global_string_farba, pridanÈ 2006-08-19; 2011-03-24: rozöÌrenÈ kvÙli dvojkombin·ci·m */
-#define MAX_QUERY_STR		2000 /* query_string */
-#define MAX_POM_QUERY_TYPE	300 /* pom_QUERY_TYPE */
-#define MAX_GLOBAL_LINK		300 /* maximalna dlzka retazca _global_link */
-//#define MAX_ENV	100 /* maximalna dlzka environmentalnej premennej */ /* 2006-08-01: nepouûÌva sa */
-#define MAX_VARIABLES		60 /* maximalny pocet syst. premennych WWW_..., zvyseny 2003-08-07 a op‰tovne 2006-08-01 a op‰ù 2011-01-26 a zas 2011-04-11 a 2011-04-12 (na 50) */
-#define MAX_GLOBAL_BUFFER	500 /* _global_buf, pridanÈ 2005-08-01 */
-/***************************************************************/
-/* EOF of former file mybase.h                                 */
-/***************************************************************/
+#define MAX_GLOBAL_STR		2048 // _global_string; 31/03/2000A.D.; 2010-12-07: rozöÌrenÈ; 2011-01-31: rozöÌrenÈ kvÙli text_JAN_31_SDB[]; 2011-02-02: rozöÌrenÈ kvÙli text_FEB_04_SJ[];
+#define MAX_GLOBAL_STR2		20 // _global_string2, pridanÈ 2006-08-19
+#define MAX_GLOBAL_STR_FARBA 350 // _global_string_farba, pridanÈ 2006-08-19; 2011-03-24: rozöÌrenÈ kvÙli dvojkombin·ci·m
+#define MAX_QUERY_STR		2000 // query_string
+#define MAX_POM_QUERY_TYPE	300 // pom_QUERY_TYPE
+#define MAX_GLOBAL_LINK		300 // maximalna dlzka retazca _global_link
+//#define MAX_ENV	100 // maximalna dlzka environmentalnej premennej | 2006-08-01: nepouûÌva sa
+#define MAX_VARIABLES		60 // maximalny pocet syst. premennych WWW_..., zvyseny 2003-08-07 a op‰tovne 2006-08-01 a op‰ù 2011-01-26 a zas 2011-04-11 a 2011-04-12 (na 50)
+#define MAX_GLOBAL_BUFFER	500 // _global_buf, pridanÈ 2005-08-01
+//***************************************************************
+//* EOF of former file mybase.h                                 *
+//***************************************************************
 
-/* Nazvy programov, suborov, skriptov... */
-#define SCRIPT_NAME          "l.cgi" /* kedysi tu bolo dnes.cgi */
+// Nazvy programov, suborov, skriptov...
+#define SCRIPT_NAME          "l.cgi"
 #define SCRIPT_PATH(a)       "cgi-bin/"a
-	/* zmenene 06/09/2001A.D.; povodne tu bolo "cgi-bin/"##a */
 
 #if defined(USE_UNCGI)
 #define UNCGI_PATH(a)        SCRIPT_PATH("uncgi.cgi/")a
 #else
 #define UNCGI_PATH(a)        SCRIPT_PATH(a)
 #endif
-	/* zmenene 06/09/2001A.D.; povodne tu bolo SCRIPT_PATH("uncgi.cgi/")##a */
-	/* 2005-03-28: Zapoznamkovane (pokus) */
 
 #define UNCGI_SCRIPT_NAME    UNCGI_PATH(SCRIPT_NAME)
 #define PATH_CGI(a) "cgi-bin/"a
-	/* zmenene 06/09/2001A.D.; povodne tu bolo "cgi-bin/"##a */
 
-/* pridane 30/03/2000A.D. */
 extern char script_name[MAX_STR]; // = cfg_HTTP_ADDRESS_default + SCRIPT_PATH(SCRIPT_NAME)
 extern char uncgi_name[MAX_STR];  // = cfg_HTTP_ADDRESS_default + UNCGI_SCRIPT_NAME
 void updateScriptName(void);
 void updateUnCGIName(void);
 
-/* tu kedysi bolo aj #define PATH(a) zvacsa a, ale nebolo to treba; vyhodene 2003-07-02 */
+// tu kedysi bolo aj #define PATH(a) zvacsa a, ale nebolo to treba; vyhodene 2003-07-02
 
 /* 2004-03-17
  * FILE_PATH nie je potrebne; citame to z myconf.cpp::readConfig() (config file)
@@ -131,68 +126,67 @@ extern char FILE_PATH[MAX_STR]; // inicializovane v breviar.cpp
 #define FILE_NAME_POKEC         MESSAGE_FOLDER"xxx"
 #define FILE_NAME_CHYBA         MESSAGE_FOLDER"chyba.htm"
 
-extern short int query_type; /* premenna obsahujuca PRM_... */
-/* deklarovana tu; definovana v dnes.cpp (18/02/2000A.D.) */
+extern short int query_type; // premenna obsahujuca PRM_...
 
-/* meno skriptu nasleduje zoznam parametrov, alebo sa precitaju z form */
-#define 	SCRIPT_PARAM_FROM_FORM     200 /* z formulara */
-#define 	SCRIPT_PARAM_FROM_QS	   201 /* z query string (sys variable) */
-#define 	SCRIPT_PARAM_FROM_ARGV	   202 /* z argumentov */
+// meno skriptu nasleduje zoznam parametrov, alebo sa precitaju z form
+#define 	SCRIPT_PARAM_FROM_FORM     200 // z formulara
+#define 	SCRIPT_PARAM_FROM_QS	   201 // z query string (sys variable)
+#define 	SCRIPT_PARAM_FROM_ARGV	   202 // z argumentov
 
-/* nasleduju typy dotazov */
+// nasleduju typy dotazov
 #define PRM_UNKNOWN		 100
 #define PRM_NONE		 110
 
-#define PRM_SIMULACIA_QS 12 /* 24/02/2000A.D. */
+#define PRM_SIMULACIA_QS 12
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_SIMULACIA_QS "PRM_SIMULACIA_QS"
 #else
 	#define STR_PRM_SIMULACIA_QS "psqs"
 #endif
 
-#define PRM_TABULKA 13 /* 15/03/2000A.D. */
+#define PRM_TABULKA 13
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_TABULKA "PRM_TABULKA"
 #else
 	#define STR_PRM_TABULKA "ptab"
 #endif
 
-#define PRM_DATUM   	0 /* dotaz na konkretny datum */
+#define PRM_DATUM   	0 // dotaz na konkretny datum
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_DATUM "PRM_DATUM"
 #else
 	#define STR_PRM_DATUM "pdt"
 #endif
 
-#define PRM_CEZ_ROK 	1    /* dotaz na napr. piatok v tretom tyzdni */
+#define PRM_CEZ_ROK 	1    // dotaz na napr. piatok v tretom tyzdni
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_CEZ_ROK "PRM_CEZ_ROK"
 #else
 	#define STR_PRM_CEZ_ROK "pcr"
 #endif
 
-#define PRM_SVIATOK 	2    /* dotaz na konkretny sviatok */
+#define PRM_SVIATOK 	2    // dotaz na konkretny sviatok
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_SVIATOK "PRM_SVIATOK"
 #else
 	#define STR_PRM_SVIATOK "psv"
 #endif
 
-#define PRM_ANALYZA_ROKU	3   /* dotaz na sviatky v roku */
+#define PRM_ANALYZA_ROKU	3   // dotaz na sviatky v roku
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_ANALYZA_ROKU "PRM_ANALYZA_ROKU"
 #else
 	#define STR_PRM_ANALYZA_ROKU "par"
 #endif
 
-#define PRM_DNES		4 /* to, co bolo kedysi bez parametra - info o dnesnom dni */
+#define PRM_DNES		4 // to, co bolo kedysi bez parametra - info o dnesnom dni
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_DNES "PRM_DNES"
 #else
 	#define STR_PRM_DNES "pdnes"
 #endif
 
-#define PRM_LIT_OBD 	5    /* podæa pÙvodnÈho PRM_CEZ_ROK */
+#define PRM_LIT_OBD 	5    // podæa pÙvodnÈho PRM_CEZ_ROK
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_CEZ_ROK "PRM_LIT_OBD"
 #else
@@ -206,7 +200,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_PRM_MESIAC_ROKA "pmr"
 #endif
 
-/* pridana konstanta pre batch mode; 2003-07-04 */
+// pridana konstanta pre batch mode; 2003-07-04
 #define PRM_BATCH_MODE 21
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_BATCH_MODE "PRM_BATCH_MODE"
@@ -235,22 +229,22 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 #define WWW_PREFIX "WWW_"
 #define ADD_WWW_PREFIX_(a) ("WWW_"a)
 
-/* nasleduju parametre z formularov pre dotaz PRM_DATUM */
-#define DEN 3 /* dotaz na konkretny datum */
+// nasleduju parametre z formularov pre dotaz PRM_DATUM
+#define DEN 3 // dotaz na konkretny datum
 #ifdef LONG_PARAM_NAMES
 	#define STR_DEN "DEN"
 #else
 	#define STR_DEN "d"
 #endif
 
-#define MESIAC 4 /* dotaz na konkretny datum */
+#define MESIAC 4 // dotaz na konkretny datum
 #ifdef LONG_PARAM_NAMES
 	#define STR_MESIAC "MESIAC"
 #else
 	#define STR_MESIAC "m"
 #endif
 
-#define ROK 5 /* dotaz na konkretny datum */
+#define ROK 5 // dotaz na konkretny datum
 #ifdef LONG_PARAM_NAMES
 	#define STR_ROK "ROK"
 #else
@@ -271,22 +265,22 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_DALSI_SVATY "ds"
 #endif
 
-/* nasleduj˙ parametre z formul·rov pre dotaz PRM_CEZ_ROK -- pouûÌva ich aj PRM_LIT_OBD */
-#define DEN_V_TYZDNI 6         /* dotaz na konkretny den */
+// nasleduj˙ parametre z formul·rov pre dotaz PRM_CEZ_ROK -- pouûÌva ich aj PRM_LIT_OBD
+#define DEN_V_TYZDNI 6         // dotaz na konkretny den
 #ifdef LONG_PARAM_NAMES
 	#define STR_DEN_V_TYZDNI "DEN_V_TYZDNI"
 #else
 	#define STR_DEN_V_TYZDNI "dvt"
 #endif
 
-#define TYZDEN	7               /* dotaz na konkretny tyzden */
+#define TYZDEN	7               // dotaz na konkretny tyzden
 #ifdef LONG_PARAM_NAMES
 	#define STR_TYZDEN "TYZDEN"
 #else
 	#define STR_TYZDEN "t"
 #endif
 
-/* nasleduju parametre z formularov pre dotaz PRM_SVIATOK */
+// nasleduju parametre z formularov pre dotaz PRM_SVIATOK
 #define NAZOV_SVIATOK 8
 #ifdef LONG_PARAM_NAMES
 	#define STR_NAZOV_SVIATOK "SVIATOK"
@@ -294,7 +288,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_NAZOV_SVIATOK "sv"
 #endif
 
-/* nasleduju parametre z formularov pre dotaz PRM_ANALYZA_ROKU */
+// nasleduju parametre z formularov pre dotaz PRM_ANALYZA_ROKU
 #define ANALYZA_ROKU 9
 #ifdef LONG_PARAM_NAMES
 	#define STR_ANALYZA_ROKU "ANALYZA_ROKU"
@@ -302,7 +296,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_ANALYZA_ROKU "ar"
 #endif
 
-/* nasleduj˙ parametre z formul·rov pre dotaz PRM_LIT_OBD */
+// nasleduj˙ parametre z formul·rov pre dotaz PRM_LIT_OBD
 #define LIT_OBD 10
 #ifdef LONG_PARAM_NAMES
 	#define STR_LIT_OBD "LIT_OBD"
@@ -317,7 +311,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_LIT_ROK "lr"
 #endif
 
-/* nasleduju parametre z formularov pre dotaz PRM_MESIAC_ROKA */
+// nasleduju parametre z formularov pre dotaz PRM_MESIAC_ROKA
 #define MESIAC_ROKA 17
 #ifdef LONG_PARAM_NAMES
 	#define STR_MESIAC_ROKA "MESIAC_ROKA"
@@ -332,7 +326,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_ROK_ROKA "rr"
 #endif
 
-/* nasleduju parametre z formularov pre dotaz PRM_TABULKA, 15/03/2000A.D. */
+// nasleduju parametre z formularov pre dotaz PRM_TABULKA
 #define ROK_FROM 40
 #ifdef LONG_PARAM_NAMES
 	#define STR_ROK_FROM "ROK_FROM"
@@ -351,10 +345,10 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 #ifdef LONG_PARAM_NAMES
 	#define STR_TABULKA_LINKY "TABULKA_LINKY"
 #else
-	#define STR_TABULKA_LINKY "hl" /* od 'hyperlinks' */
+	#define STR_TABULKA_LINKY "hl" // od 'hyperlinks'
 #endif
 
-#define PRM_DETAILY 30 /* dotaz na konkretny datum, to iste ako PRM_DATUM */
+#define PRM_DETAILY 30 // dotaz na konkretny datum, to iste ako PRM_DATUM
 #ifdef LONG_PARAM_NAMES
 	#define STR_PRM_DETAILY "PRM_DETAILY"
 #else
@@ -401,7 +395,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_MODL_OPT4 "o4"
 #endif
 
-/* append pridany 2003-07-08, bude v _global_opt_append; nemal by byt pouzity na webe */
+// append pridany 2003-07-08, bude v _global_opt_append; nemal by byt pouzity na webe
 #define MODL_OPT_APPEND 35
 #ifdef LONG_PARAM_NAMES
 	#define STR_MODL_OPT_APPEND "MODL_OPT_APPEND"
@@ -409,7 +403,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_MODL_OPT_APPEND "a"
 #endif
 
-/* 2006-07-11: PridanÈ kvÙli jazykov˝m mut·ci·m */
+// 2006-07-11: PridanÈ kvÙli jazykov˝m mut·ci·m
 #define JAZYK 37
 #ifdef LONG_PARAM_NAMES
 	#define STR_JAZYK "JAZYK"
@@ -417,7 +411,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_JAZYK "j"
 #endif
 
-/* --- force verzie pre option 0 aû 4 --- */
+// --- force verzie pre option 0 aû 4 ---
 #define MODL_OPTF0 50
 #ifdef LONG_PARAM_NAMES
 	#define STR_MODL_OPTF0 "MODL_OPTF0"
@@ -453,7 +447,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_MODL_OPTF4 "of4"
 #endif
 
-/* pre force option 0 jednotlivÈ bit-komponenty (parci·lne voæby) */
+// pre force option 0 jednotlivÈ bit-komponenty (parci·lne voæby)
 
 // ËÌslovanie (ËÌsla) veröov
 #define MODL_OPTF0_VERSE 61
@@ -479,7 +473,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_MODL_OPTF0_CIT "of0cit"
 #endif
 
-/* pre force option 1 jednotlivÈ bit-komponenty (parci·lne voæby) */
+// pre force option 1 jednotlivÈ bit-komponenty (parci·lne voæby)
 
 // evanjeliovÈ chv·lospevy (benediktus, magnifikat, nunk dimittis)
 #define MODL_OPTF1_CHV 71
@@ -577,7 +571,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_MODL_OPTF1_PROSBY_ZVOLANIE "of1prz"
 #endif
 
-/* pre force option 2 jednotlivÈ bit-komponenty (parci·lne voæby) */
+// pre force option 2 jednotlivÈ bit-komponenty (parci·lne voæby)
 
 // ISO form·t d·tumu
 #define MODL_OPTF2_ISO_DATUM 81
@@ -643,7 +637,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_MODL_OPTF2_BUTTONY_USPORNE "of2btnu"
 #endif
 
-/* 2008-08-08: PridanÈ kvÙli rÙznym css */
+// 2008-08-08: PridanÈ kvÙli rÙznym css
 #define CSS 40
 #ifdef LONG_PARAM_NAMES
 	#define STR_CSS "CSS"
@@ -651,7 +645,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_CSS "c"
 #endif
 
-/* 2008-11-29: PridanÈ: spÙsob zapisovania d·tumu pre s˙bory v batch mÛde */
+// 2008-11-29: PridanÈ: spÙsob zapisovania d·tumu pre s˙bory v batch mÛde
 #define MODL_OPT_DATE_FORMAT 41
 #ifdef LONG_PARAM_NAMES
 	#define STR_MODL_OPT_DATE_FORMAT "MODL_OPT_DATE_FORMAT"
@@ -659,7 +653,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_MODL_OPT_DATE_FORMAT "u"
 #endif
 
-/* 2010-08-04: PridanÈ kvÙli jazykov˝m mut·ci·m -- kalend·r (napr. rehoæn˝) */
+// 2010-08-04: PridanÈ kvÙli jazykov˝m mut·ci·m -- kalend·r (napr. rehoæn˝)
 #define KALENDAR 42
 #ifdef LONG_PARAM_NAMES
 	#define STR_KALENDAR "KALENDAR"
@@ -667,7 +661,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_KALENDAR "k"
 #endif
 
-/* 2011-05-06: pridanÈ */
+// n·zov fontu
 #define FONT_NAME 43
 #ifdef LONG_PARAM_NAMES
 	#define STR_FONT_NAME "FONT_NAME"
@@ -675,7 +669,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_FONT_NAME "f"
 #endif
 
-/* 2011-05-13: pridanÈ */
+// n·zov veækosti fontu
 #define FONT_SIZE 44
 #ifdef LONG_PARAM_NAMES
 	#define STR_FONT_SIZE "FONT_SIZE"
@@ -683,8 +677,7 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 	#define STR_FONT_SIZE "s"
 #endif
 
-
-/* global */
+// global
 #define QUERY_TYPE	20
 #ifdef LONG_PARAM_NAMES
 	#define STR_QUERY_TYPE "QUERY_TYPE"
@@ -744,39 +737,39 @@ extern short int query_type; /* premenna obsahujuca PRM_... */
 #define HTML_CLASS_TT       "class=\"tt\""
 #define HTML_CLASS_BUTTON   "class=\"button\""
 
-/* pridane 2003-07-09, zmenene v cestach pre skript */
+// pridane 2003-07-09, zmenene v cestach pre skript
 #define HTML_AMPERSAND          "&amp;"
-/* 2006-08-19: pridanÈ */
+
 #define HTML_COMMENT_BEGIN		"<!--"
 #define HTML_COMMENT_END		"-->"
 #define HTML_FONT_SIZE_FARBA	"2"
-/* 2007-03-19: pridanÈ namiesto gt a lt html znakov */
+// 2007-03-19: pridanÈ namiesto gt a lt html znakov
 #define HTML_LEFT_ARROW			"&laquo;"
 #define HTML_RIGHT_ARROW		"&raquo;"
-/* 2009-08-12: pridanÈ */
+
 #define HTML_LINK_CALL1         "%s?%s=%s"HTML_AMPERSAND"%s=%d"HTML_AMPERSAND"%s=%d"HTML_AMPERSAND"%s=%d%s"
 #define HTML_LINK_CALL2         "%s?%s=%s"HTML_AMPERSAND"%s=%s"HTML_AMPERSAND"%s=%d"HTML_AMPERSAND"%s=%d%s"
-/* 2011-01-26: pridanÈ jednoduchÈ öÌpky væavo/vpravo */
+// 2011-01-26: pridanÈ jednoduchÈ öÌpky væavo/vpravo
 #define HTML_LEFT_ARROW_SINGLE	"&lsaquo;"
 #define HTML_RIGHT_ARROW_SINGLE	"&rsaquo;"
-/* 2011-01-26: pridanÈ "trojitÈ" öÌpky væavo/vpravo */
+// 2011-01-26: pridanÈ "trojitÈ" öÌpky væavo/vpravo
 #define HTML_LEFT_ARROW_HUGE	"&lsaquo;&laquo;"
 #define HTML_RIGHT_ARROW_HUGE	"&raquo;&rsaquo;"
 
-/* 2007-03-19: v˝pis "Dnes je..." sa zobrazÌ len pri tomto nastavenÌ, ak je 1 */
+// 2007-03-19: v˝pis "Dnes je..." sa zobrazÌ len pri tomto nastavenÌ, ak je 1
 #define HTML_ZOBRAZIT_DNES_JE	0
 
-/* 2007-06-01: niektorÈ definy presunutÈ z liturgia.h */
+// 2007-06-01: niektorÈ definy presunutÈ z liturgia.h
 
 // option 2
 #define MODL_ZALMY_ZO_DNA 0
 #define MODL_ZALMY_ZO_SV  1
 
-/* option 5, 2003-08-06, upravena 2003-08-13 */
+// option 5, 2003-08-06, upravena 2003-08-13
 #define MODL_CEZ_DEN_ZALMY_ZO_DNA         0
 #define MODL_CEZ_DEN_DOPLNKOVA_PSALMODIA  1
 
-/* 2008-11-29: pridanÈ rÙzne moûnosti batch exportu */
+// 2008-11-29: pridanÈ rÙzne moûnosti batch exportu
 #define	EXPORT_DATE_SIMPLE 0
 #define	EXPORT_DATE_FULL   1
 
