7870e1c18bb8822c7a6b4dbab7180da7ac10a94b
Juraj Vidéky (WT) <videky@breviar.sk>
2011-11-08 13:09:16 +0100

    merge so zmenami pre android (Richard Královič)

--- cgi-bin/android/src/sk/breviar/android/Breviar.java
+++ cgi-bin/android/src/sk/breviar/android/Breviar.java
@@ -1,9 +1,13 @@
 package sk.breviar.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Intent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.DialogInterface;
+import android.app.Dialog;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.net.Uri;
@@ -23,15 +27,17 @@ import android.widget.Button;
 import java.io.IOException;
 import sk.breviar.android.Server;
 
-public class Breviar extends Activity
-{
+public class Breviar extends Activity {
+    static String scriptname = "cgi-bin/l.cgi";
+    static final String prefname = "BreviarPrefs";
+    static final int DIALOG_HU_BETA = 1;
+    static final int DIALOG_NEWS = 2;
+
     WebView wv;
     int scale;
     Server S = null;
-    static String scriptname = "cgi-bin/l.cgi";
     String language;
-    static final String prefname = "BreviarPrefs";
-    boolean initialized;
+    boolean initialized, clearHistory;
 
     void goHome() {
       wv.loadUrl("http://localhost:" + S.port + "/" + scriptname + "?qt=pdnes" + Html.fromHtml(S.getOpts()));
@@ -40,7 +46,9 @@ public class Breviar extends Activity
     void resetLanguage() {
       S.setLanguage(language);
       // musime zahodit aj nastavenia, mozu byt ine. A zresetovalo by to aj jazyk spat.
-      wv.loadUrl("http://localhost:" + S.port + "/" + scriptname + "?qt=pdnes" );
+      clearHistory = true;
+      wv.loadUrl("http://localhost:" + S.port + "/" + scriptname + "?qt=pdnes");
+      syncPreferences();
     }
 
     public boolean tryOpenBible(String url) {
@@ -124,6 +132,13 @@ public class Breviar extends Activity
           if (parent.initialized) parent.syncScale();
           parent.initialized = true;
         }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+          //Log.v("breviar", "onPageFinished");
+          if (parent.clearHistory) view.clearHistory();
+          parent.clearHistory = false;
+        }
       } );
 
       ((Button)findViewById(R.id.forwardBtn)).setOnClickListener(new View.OnClickListener() {
@@ -151,7 +166,11 @@ public class Breviar extends Activity
           wv.pageDown(false);
         }
       });
- 
+
+      if (!getResources().getString(R.string.version).equals(settings.getString("version", ""))) {
+        showDialog(DIALOG_NEWS);
+        markVersion();
+      }
     }
 
     protected void onSaveInstanceState(Bundle outState) {
@@ -173,6 +192,13 @@ public class Breviar extends Activity
       }
 
       // Commit the edits!
+      editor.commit();
+    }
+
+    void markVersion() {
+      SharedPreferences settings = getSharedPreferences(prefname, 0);
+      SharedPreferences.Editor editor = settings.edit();
+      editor.putString("version", getResources().getString(R.string.version));
       editor.commit();
     }
 
@@ -204,6 +230,35 @@ public class Breviar extends Activity
     }
 
     @Override
+    protected Dialog onCreateDialog(int id) {
+      switch(id) {
+        case DIALOG_HU_BETA:
+          return new AlertDialog.Builder(this)
+                 .setMessage(R.string.hu_beta_warning)
+                 .setCancelable(false)
+                 .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                     public void onClick(DialogInterface dialog, int id) {
+                          dialog.cancel();
+                     }
+                 })
+                 .create();
+        case DIALOG_NEWS:
+          return new AlertDialog.Builder(this)
+                 .setMessage(R.string.news)
+                 .setCancelable(false)
+                 .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                     public void onClick(DialogInterface dialog, int id) {
+                          dialog.cancel();
+                     }
+                 })
+                 .create();
+        default:
+          // fall through
+      }
+      return null;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
       // Handle item selection
       switch (item.getItemId()) {
@@ -214,6 +269,11 @@ public class Breviar extends Activity
         case R.id.lang_cz:
           language = "cz";
           resetLanguage();
+          return true;
+        case R.id.lang_hu:
+          language = "hu";
+          resetLanguage();
+          showDialog(DIALOG_HU_BETA);
           return true;
         default:
           return super.onOptionsItemSelected(item);
