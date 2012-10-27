114554f70f6be51c751cc19890eef1a33ccca56c
Richard Kralovic <riso@ksp.sk>
2012-10-27 20:21:21 +0200

    Fix notifications

--- android/src/sk/breviar/android/AlarmTimePickerFragment.java
+++ android/src/sk/breviar/android/AlarmTimePickerFragment.java
@@ -34,42 +34,11 @@ public class AlarmTimePickerFragment extends DialogFragment
     TimePickerDialog d = new TimePickerDialog(getActivity(), this,
         t.hour, t.minute, DateFormat.is24HourFormat(getActivity()));
 
-    /*
-    d.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel),
-        new TimePickerDialog.OnClickListener() {
-          public void onClick(DialogInterface dialog, int which) {
-            event.disable(getActivity());
-//            box.setChecked(true); // will be toggled to false
-          }
-        });
-        */
-
-    Button b;
-    b = d.getButton(DialogInterface.BUTTON_NEGATIVE);
-    if (b != null) b.setEnabled(false);
-
-    b = d.getButton(DialogInterface.BUTTON_NEUTRAL);
-    if (b != null) b.setEnabled(false);
-
-    /*
-    d.setOnDismissListener(new TimePickerDialog.OnDismissListener() {
-          public void onDismiss(DialogInterface dialog) {
-            box().setChecked(!box().isChecked());
-          }
-        });
-
-    d.setOnCancelListener(new TimePickerDialog.OnCancelListener() {
-          public void onCancel(DialogInterface dialog) {
-            box().setChecked(!box().isChecked());
-          }
-        });
-    */
-
     return d;
   }
 
   public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-    // box.setChecked(true);
+    box().setChecked(true);
     Util.EventInfo e = event();
     e.setTime(getActivity(), hourOfDay, minute);
     e.updateBox(box(), new Util.AlarmTime(minute, hourOfDay, true));
