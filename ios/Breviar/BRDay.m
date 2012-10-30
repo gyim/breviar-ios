24656fc209f3c7126ccfe22dc1261f22362a6f3d
Akos Gyimesi <gyimesi.akos@gmail.com>
2012-10-31 00:02:43 +0100

    parse celebration data from XML output

--- ios/Breviar/BRDay.m
+++ ios/Breviar/BRDay.m
@@ -8,6 +8,7 @@
 
 #import "BRDay.h"
 #import "BRCGIQuery.h"
+#import "RaptureXML/RXMLElement.h"
 
 @implementation BRDay
 
@@ -25,11 +26,36 @@
 				   @"d": [NSNumber numberWithInteger:components.day],
 				   @"m": [NSNumber numberWithInteger:components.month],
 				   @"r": [NSNumber numberWithInteger:components.year],
-				   @"j": @"hu"
+				   @"j": @"hu",
 				   }];
 		
-		// Parse XML data (TODO)
-		NSLog(@"XML data:\n%@", dayData);
+		// Find XML header
+		NSRange r = [dayData rangeOfString:@"<?xml"];
+		if (r.location != NSNotFound) {
+			dayData = [dayData substringFromIndex:r.location];
+		}
+		dayData = [dayData stringByAppendingString:@"</LHData>"]; // HACK
+		
+		// Parse XML data
+		RXMLElement *xml = [RXMLElement elementFromXMLString:dayData encoding:NSWindowsCP1250StringEncoding];
+		NSMutableArray *celebrations = [NSMutableArray array];
+		
+		[xml iterate:@"CalendarDay.Celebration" usingBlock:^(RXMLElement *celebrationXml) {
+			BRCelebration *celebration = [[BRCelebration alloc] init];
+			celebration.celebrationId = [celebrationXml attributeAsInt:@"Id"];
+			
+			// TODO: turn HTML code into separate XML elements
+			[celebrationXml iterate:@"StringTitle.span" usingBlock:^(RXMLElement *node) {
+				NSString *value = node.description;
+				if (!celebration.title)
+					celebration.title = value;
+				else
+					celebration.subtitle = value;
+			}];
+			[celebrations addObject:celebration];
+		}];
+		
+		self.celebrations = [NSArray arrayWithArray:celebrations];
 	}
 	return self;
 }
