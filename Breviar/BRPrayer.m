edfd9234e02abaa045abb84fe26e8e7235367b5f
Akos Gyimesi <gyimesi.akos@gmail.com>
2012-10-30 23:10:02 +0100

    refactor: add BRDay and BRCelebration for XML day data parsing (coming later)

---
+++ Breviar/BRPrayer.m
@@ -0,0 +1,60 @@
+//
+//  BRPrayer.m
+//  Breviar
+//
+//  Created by Gyimesi Akos on 9/7/12.
+//
+//
+
+#import "BRPrayer.h"
+#include "BRCGIQuery.h"
+
+@implementation BRPrayer
+@synthesize prayerType;
+@synthesize title;
+@synthesize body;
+
+static NSString *prayerTitles[] = {
+    [BRInvitatory]         = @"Invitatory",
+    [BROfficeOfReadings]   = @"Office of Readings",
+    [BRMorningPrayer]      = @"Morning Prayer",
+    [BRMidMorningPrayer]   = @"Mid-Morning Prayer",
+    [BRMiddayPrayer]       = @"Midday Prayer",
+    [BRMidAfternoonPrayer] = @"Mid-Afternoon Prayer",
+    [BREveningPrayer]      = @"Evening Prayer",
+    [BRCompline]           = @"Compline"
+};
+
+static NSString *prayerQueryIds[] = {
+    [BRInvitatory]         = @"mi",
+    [BROfficeOfReadings]   = @"mpc",
+    [BRMorningPrayer]      = @"mrch",
+    [BRMidMorningPrayer]   = @"mpred",
+    [BRMiddayPrayer]       = @"mna",
+    [BRMidAfternoonPrayer] = @"mpo",
+    [BREveningPrayer]      = @"mv",
+    [BRCompline]           = @"mk"
+};
+
++ (BRPrayer *)prayerWithType:(BRPrayerType)prayerType celebration:(NSInteger)celebrationId date:(NSDate *)date {
+    BRPrayer *prayer = [[BRPrayer alloc] init];
+    prayer.title = prayerTitles[prayerType];
+    
+    // Get date components
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit fromDate:date];
+    
+    prayer.body = [BRCGIQuery queryWithArgs:@{
+                   @"qt": @"pdt",
+                   @"d": [NSNumber numberWithInteger:components.day],
+                   @"m": [NSNumber numberWithInteger:components.month],
+                   @"r": [NSNumber numberWithInteger:components.year],
+                   @"p": prayerQueryIds[prayerType],
+                   @"ds": [NSNumber numberWithInteger:celebrationId],
+                   @"o0": @"60",
+                   @"o2": @"152",
+                   }];
+    
+    return prayer;
+}
+
+@end
