5eb304ed9b576fa194ad828f21a1147918e0cc81
Juraj Vidéky (WT) <videky@breviar.sk>
2013-01-09 17:42:12 +0100

    zmena (c) info rok 2012->2013

--- cgi-bin/source/hodin.h
+++ cgi-bin/source/hodin.h
@@ -1,7 +1,7 @@
 /***************************************************************/
 /*                                                             */
 /* hodin.h                                                     */
-/* (c)1999-2012 | Juraj Videky | videky@breviar.sk             */
+/* (c)1999-2013 | Juraj Vidéky | videky@breviar.sk             */
 /*                                                             */
 /* description | definovane stringove kontanty                 */
 /* document history                                            */
