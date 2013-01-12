45116571ef37435e4609775a11c0e8a3e0a1a9f7
Akos Gyimesi <gyimesi.akos@gmail.com>
2013-01-12 19:46:31 +0100

    iOS: show liturgical color in celebration selection

--- ios/Breviar/BRCelebration.h
+++ ios/Breviar/BRCelebration.h
@@ -8,11 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
+// See LIT_FARBA_... constants in liturgia.h
+typedef enum {
+	BRColorUnknown       = 0,
+	BRColorRed           = 1,
+	BRColorWhite         = 2,
+	BRColorGreen         = 3,
+	BRColorViolet        = 4,
+	BRColorRose          = 5,
+	BRColorBlack         = 6,
+	BRColorVioletOrBlack = 7,
+	BRColorVioletOrWhite = 8,
+	BRColorRoseOrViolet  = 9
+} BRLiturgicalColor;
+
 @interface BRCelebration : NSObject
 
 @property NSInteger celebrationId;
 @property(strong) NSString *title;
 @property(strong) NSString *subtitle;
 @property(strong) NSArray *prayers;
+@property(assign) BRLiturgicalColor liturgicalColor;
 
 @end
