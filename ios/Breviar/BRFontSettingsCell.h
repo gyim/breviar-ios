f139133a2085d8380b82b576291eed127cde8640
Akos Gyimesi <gyimesi.akos@gmail.com>
2013-01-27 13:50:35 +0100

    iOS: prayer settings / show prayer parts

---
+++ ios/Breviar/BRFontSettingsCell.h
@@ -0,0 +1,13 @@
+//
+//  BRFontSettingsCell.h
+//  Breviar
+//
+//  Created by Gyimesi Akos on 1/26/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRFontSettingsCell : UITableViewCell
+@property(weak,nonatomic) IBOutlet UILabel *fontLabel;
+@end
--- ios/Breviar/BRSettings.m
+++ ios/Breviar/BRSettings.m
@@ -20,6 +20,21 @@ static BRSettings *_instance;
 	return _instance;
 }
 
+- (id)init {
+	if (self = [super init]) {
+		self.defaultOptions = [NSSet setWithObjects:
+							   // Enable all prayer parts by default
+							   @"of1c", @"of1s", @"of1r", @"of1o", @"of1t", @"of1pr",
+							   nil];
+		
+		self.allOptions = [NSSet setWithObjects:
+						   // Enabled prayer parts
+						   @"of1c", @"of1s", @"of1r", @"of1o", @"of1t", @"of1pr",
+						   nil];
+	}
+	return self;
+}
+
 - (NSString *)prayerFontFamily {
 	NSString *value = [[NSUserDefaults standardUserDefaults] stringForKey:@"PrayerFontFamily"];
 	return value ? value : @"Baskerville";
@@ -36,6 +51,34 @@ static BRSettings *_instance;
 
 - (void)setPrayerFontSize:(NSInteger)prayerFontSize {
 	[[NSUserDefaults standardUserDefaults] setInteger:prayerFontSize forKey:@"PrayerFontSize"];
+}
+
+- (BOOL)boolOption:(NSString *)optionId {
+	NSUserDefaults *settings = [NSUserDefaults standardUserDefaults];
+	if ([settings objectForKey:optionId]) {
+		return [settings boolForKey:optionId];
+	}
+	else {
+		return [self.defaultOptions containsObject:optionId];
+	}
+}
+
+- (void)setBoolOption:(NSString *)optionId value:(BOOL)value {
+	[[NSUserDefaults standardUserDefaults] setBool:value forKey:optionId];
+}
+
+- (NSDictionary *)prayerQueryOptions {
+	NSMutableDictionary *opts = [NSMutableDictionary dictionary];
+	
+	for (NSString *optionId in self.allOptions) {
+		if ([self boolOption:optionId]) {
+			[opts setObject:@"1" forKey:optionId];
+		}
+		else {
+			[opts setObject:@"0" forKey:optionId];
+		}
+	}
+	return opts;
 }
 
 @end
