b2cd8c2b97c8438bff7cd3f436764b388bf7b774
Richard Kralovic <riso@kralovic.org>
2013-03-03 22:20:35 +0100

    Work on the testing infrastructure

--- android/testing/run_tests.py
+++ android/testing/run_tests.py
@@ -4,7 +4,17 @@ import time;
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice;
 
 def orangutan_click(x, y):
-  os.system("adb shell \"echo 'tap 186 408 1 200' | /data/orng /dev/input/event0\"");
+  os.system("adb shell \"echo 'tap %d %d 1 200' | /data/orng /dev/input/event0\"" % (x, y));
+
+def stable_snapshot(file):
+  stableImage = False 
+  while not stableImage: 
+      result = device.takeSnapshot() 
+      MonkeyRunner.sleep(1) 
+      result2 = device.takeSnapshot() 
+      stableImage = result2.sameAs(result, 0.5) 
+  #MonkeyRunner.sleep(shortSleepInterval) 
+  result2.writeToFile(file,'png') 
 
 if len(sys.argv) < 5:
   print "Usage: run_tests.py <dir> <date> <commit id> <version>"
@@ -20,43 +30,52 @@ runComponent = package + '/' + activity
 
 device.wake()
 
+#set fixed date
+os.system("adb shell 'date 1362343193'")
+
 print "Starting activity"
 device.startActivity(component=runComponent)
+time.sleep(3)
 
-time.sleep(5);
-result = device.takeSnapshot()
-result.writeToFile(root_dir + '/changelog.png','png')
+stable_snapshot(root_dir + '/changelog.png')
 
 # try to find OK in changelog. We do not need orangutan here.
 for i in [1200, 1100, 1000, 900, 800, 700]:
   device.touch(360, i, MonkeyDevice.DOWN_AND_UP)
 
-time.sleep(2);
-result = device.takeSnapshot()
-result.writeToFile(root_dir + '/start.png','png')
+stable_snapshot(root_dir + '/start.png')
 
 # Vespery
 # Webview ignores monkey events, so we must use orangutan.
 orangutan_click(186, 408)
 
-time.sleep(5);
-result = device.takeSnapshot()
-result.writeToFile(root_dir + '/vespery.png','png')
+stable_snapshot(root_dir + '/vespers.png')
 
 # Presses the Menu button
 device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
-
+time.sleep(1)
 orangutan_click(128, 1122)
 
-time.sleep(3);
-result = device.takeSnapshot()
-result.writeToFile(root_dir + '/menu.png','png')
+stable_snapshot(root_dir + '/menu.png')
+
+# Test language switch (Czech)
+orangutan_click(40, 500)
+orangutan_click(40, 600)
+orangutan_click(300, 200)
+
+stable_snapshot(root_dir + '/start-cz.png')
+
+orangutan_click(186, 408)
+
+stable_snapshot(root_dir + '/vespers-cz.png')
+
 
 device.removePackage('sk.breviar.android')
 
 date_str = sys.argv[2]
 commit_id = sys.argv[3]
 version = sys.argv[4]
+
 out = open(root_dir + '/index.html', 'w')
 out.write("""
 <html>
@@ -72,13 +91,46 @@ out.write("""
 <tr> <td>Ran on:</td><td>%s</td> </tr>
 </table>
 
+<p>
+Links: <a href="../..">all nightlies</a>, <a href="../../releases">all releases</a>
+
 <h2>Screenshots</h2>
 
 <p> Changelog: <img valign=center height=600 src="changelog.png">
 <p> Main screen: <img valign=center height=600 src="start.png">
+<p> Vespers: <img valign=center height=600 src="vespers.png">
 <p> Menu: <img valign=center height=600 src="menu.png">
-<p> Vespers: <img valign=center height=600 src="vespery.png">
+<p> Main screen, CZ: <img valign=center height=600 src="start-cz.png">
+<p> Vespers, CZ: <img valign=center height=600 src="vespers-cz.png">
+
+<h2>Make release</h2>
+<p>
+<a href="release.php">Release this nightly!</a>
+
 </body>
 </html>""" % (date_str, date_str, commit_id, version,
               time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) ));
 out.close();
+
+out = open(root_dir + '/release.php', 'w')
+out.write("""
+<html>
+<head>
+<title>Releasing %s</title>
+</head>
+<body>
+<?php
+$dir = "%s/../../../releases/%s";
+if (is_dir($dir)) {
+  echo "This version already exists in releases!";
+  die(0);
+} else {
+  exec("rm -rf $dir; mkdir -p $dir; cp -R %s/.. $dir; chmod -R go+rX $dir");
+}
+?>
+Released, check 
+<a href="../../../releases">here.</a>
+</body>
+</html>""" % (date_str, root_dir, version, root_dir));
+out.close();
+
