164293a150e22fd7caae3292e51bc027bfa52fb2
Juraj Vidéky (HR) <videky@breviar.sk>
2013-06-16 22:32:48 +0200

    .

--- cgi-bin/source/breviar.cpp
+++ cgi-bin/source/breviar.cpp
@@ -2061,7 +2061,8 @@ void includeFile(short int type, const char *paramname, const char *fname, const
 					// 2011-04-04: zobraziù/nezobraziù ËÌslovanie veröov
 					if(equals(strbuff, PARAM_CISLO_VERSA_BEGIN) && (vnutri_inkludovaneho == 1)){
 						if((_global_opt[OPT_0_SPECIALNE] & BIT_OPT_0_VERSE) == BIT_OPT_0_VERSE){
-							Export("</b><"HTML_SUP_RED">");
+							// Export("</b><"HTML_SUP_RED">");
+							Export("<sup>");
 						}
 						else{
 							write = NIE;
@@ -2385,7 +2386,8 @@ void interpretParameter(short int type, char *paramname, short int aj_navigacia 
 	if(equals(paramname, PARAM_CISLO_VERSA_BEGIN)){
 		if(_global_skip_in_prayer != ANO){
 			if((_global_opt[OPT_0_SPECIALNE] & BIT_OPT_0_VERSE) == BIT_OPT_0_VERSE){
-				Export("</b><"HTML_SUP_RED">");
+				// Export("</b><"HTML_SUP_RED">");
+				Export("<sup>");
 			}
 			else{
 				// Log("  ruöÌm writing to export file, kvÙli PARAM_CISLO_VERSA_BEGIN...\n");
