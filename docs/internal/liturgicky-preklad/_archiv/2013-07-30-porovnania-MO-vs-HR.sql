ddab116c82c21d9d313e0a5f12494a9e89189031
Juraj Vidéky (WT) <videky@breviar.sk>
2013-07-30 13:01:43 +0200

    LP: opravy, porovnania

--- docs/internal/liturgicky-preklad/_archiv/2013-07-30-porovnania-MO-vs-HR.sql
+++ docs/internal/liturgicky-preklad/_archiv/2013-07-30-porovnania-MO-vs-HR.sql
@@ -17,7 +17,12 @@ where
 ;
 go
 
-select mo.nvg, hr.nvg, mo.*, hr.*
+-- MO = WT, Moskovská 13
+-- HR = doma, Hrobákova 26
+
+-- porovnanie databáz na nezrovnalosti / po úprave na html text
+
+select mo.nvg_html, hr.nvg_html, mo.*, hr.*
 from dkc..biblia mo
 join dkchr..biblia hr
 	on	mo.id = hr.id
@@ -25,4 +30,21 @@ join dkchr..biblia hr
 	and		hr.vers = 1
 	and		mo.vers = 1
 where 
-mo.spis = 'Mud' and mo.k = 18 and mo.v in (6, 16)
+		isnull(cast(mo.nvg_html as nvarchar(max)), N'') != isnull(cast(hr.nvg_html as nvarchar(max)), N'')
+		or	isnull(mo.lc_id, 0) != isnull(hr.lc_id, 0)
+		or	isnull(mo.lh_id, N'') != isnull(hr.lh_id, N'')
+;
+go
+
+/*
+-- fix
+update mo
+set mo.nvg_html = hr.nvg_html
+from dkc..biblia mo
+join dkchr..biblia hr
+	on	mo.id = hr.id
+	and		mo.spis = hr.spis
+	and		hr.vers = 1
+	and		mo.vers = 1
+where	isnull(cast(mo.nvg_html as nvarchar(max)), N'') != isnull(cast(hr.nvg_html as nvarchar(max)), N'')
+*/
