b636ae3ceb2d79cc57ff2efbf49920dac2045048
Richard Kralovic <riso@ksp.sk>
2013-12-09 16:45:55 +0100

    Revert "Revert me: dump debug output"
    This reverts commit ba53b4dd967548d47f2979e8ea5755f22dae39a5.

--- android/src/sk/breviar/android/Copy.java
+++ android/src/sk/breviar/android/Copy.java
@@ -2,7 +2,6 @@ package sk.breviar.android;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.lang.Thread;
 import android.content.Context;
@@ -25,28 +24,20 @@ public class Copy extends Thread
     }
 
     static int maxLen = 32*1024;
-
-    static int fileNum = 0;
-
     public void run() {
       byte[] buf;
       int len;
 
       buf = new byte[maxLen];
 
-      fileNum++;
-
       try {
-        FileOutputStream fo = new FileOutputStream("/data/data/sk.breviar.android/debug" + fileNum);
         while (true) {
           // Log.v("breviar: Copy: " + inst, "reading");
           len = in.read(buf, 0, maxLen);
           if (len==-1) break;
           // Log.v("breviar: Copy:" + inst, "writing " + len + " bytes: " + new String(buf, 0, len, "ISO-8859-1"));
           out.write(buf, 0, len);
-          fo.write(buf, 0, len);
         }
-        fo.close();
       } catch (IOException e) {
         Log.v("Breviar: Copy:", "IOException "+e.getMessage());
       } catch (IndexOutOfBoundsException e) {
