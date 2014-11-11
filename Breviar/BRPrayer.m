9c7f741863cd678e193180d8ac6c918d3e0edf36
Juraj Vidéky (WT) <videky@breviar.sk>
2014-11-11 16:10:35 +0100

    preparation for new option 'k' (issue # 14)

--- Breviar/BRPrayer.m
+++ Breviar/BRPrayer.m
@@ -43,6 +43,42 @@ static NSString *prayerNames[] = {
     [BRCompline]           = @"Compline"
 };
 
+static NSString *propriaCalendarIds[] = {
+    [BRCalSKGeneral]          = @"sk",
+    [BRCalCZGeneral]          = @"cz",
+    [BRCalCZ_OP]              = @"czop",
+    [BRCalSK_CSSR]            = @"cssr",
+    [BRCalHUGeneral]          = @"hu",
+    [BRCalSK_SVD]             = @"svd",
+    [BRCalSK_SJ]              = @"sj",
+    [BRCalSK_SDB]             = @"sdb",
+    [BRCalSK_OFM]             = @"ofm",
+    [BRCalSK_OP]              = @"op",
+    [BRCalSK_CM]              = @"cm",
+    [BRCalCZ_OPRAEM]          = @"opraem",
+    [BRCalCZ_OFMCAP]          = @"ofmcap",
+    [BRCalCZ_CSSR]            = @"czcssr",
+    [BRCalCZ_SDB]             = @"czsdb"
+};
+
+static NSString *propriaCalendarNames[] = {
+    [BRCalSKGeneral]          = @"vöeobecn˝ [pre Slovensko]",
+    [BRCalCZGeneral]          = @"obecn˝ [pro »echy a Moravu]",
+    [BRCalCZ_OP]              = @"CZ OP", // not used
+    [BRCalSK_CSSR]            = @"pre Kongreg·ciu najsv. Vykupiteæa ó redemptoristov (CSsR)",
+    [BRCalHUGeneral]          = @"·ltal·nos [Magyarorsz·g]",
+    [BRCalSK_SVD]             = @"pre SpoloËnosù Boûieho Slova ó verbistov (SVD)",
+    [BRCalSK_SJ]              = @"pre SpoloËnosù Jeûiöovu ó jezuitov (SJ)",
+    [BRCalSK_SDB]             = @"pre salezi·nsku rodinu ó SDB, FMA, ASC",
+    [BRCalSK_OFM]             = @"pre frantiök·nsku rodinu",
+    [BRCalSK_OP]              = @"pre Rehoæu kazateæov ó dominik·nov (OP)",
+    [BRCalSK_CM]              = @"pre Misijn˙ spoloËnosù sv. Vincenta de Paul ó lazaristov (CM)",
+    [BRCalCZ_OPRAEM]          = @"s vlastnÌmi texty premonstr·tskÈho ¯·du (OPraem)"
+    [BRCalCZ_OFMCAP]          = @"s vlastnÌmi kapucÌnsk˝mi texty (OFMCap)",
+    [BRCalCZ_CSSR]            = @"s vlastnÌmi texty Kongregace nejsv. Vykupitele ó redemptorist˘ (CSsR)",
+    [BRCalCZ_SDB]             = @"s vlastnÌmi texty pro salesi·nskou rodinu" ,
+};
+
 - (NSString *)title {
     return BREVIAR_STR(prayerQueryIds[self.prayerType]);
 }
