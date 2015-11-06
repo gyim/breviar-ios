e16475b0bf16daf839a862b8c4cfdc1b117cac22
Akos Gyimesi <akos@gyim.hu>
2015-11-06 23:09:14 +0100

    make schemes shared; use xctool in build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -e
 
 time (
 	for lang in cs hu sk; do
-		xcodebuild -project Breviar.xcodeproj  -scheme "Breviar-$lang" -configuration "Breviar-$lang" clean
-		xcodebuild -project Breviar.xcodeproj  -scheme "Breviar-$lang" -configuration "Breviar-$lang" archive
+		xctool -workspace Breviar.xcworkspace -scheme "Breviar-$lang" -configuration "Breviar-$lang" clean
+		xctool -workspace Breviar.xcworkspace -scheme "Breviar-$lang" -configuration "Breviar-$lang" archive
 	done
 )
