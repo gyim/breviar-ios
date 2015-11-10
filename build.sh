08fa6fb5cd1b86fe3cacdd06af418a138a2b182b
Akos Gyimesi <akos@gyim.hu>
2015-11-10 23:20:23 +0100

    build.sh: export archives as ad-hoc IPAs

--- build.sh
+++ build.sh
@@ -3,12 +3,22 @@
 
 set -e
 
+if [[ -z $1 ]]
+then
+	echo "Usage: build.sh <version>"
+	exit 1
+fi
+
+version=$1
+
 ./buildno.sh
 
 time (
 	for lang in cs hu sk; do
-		rm -rf build/Breviar-$lang.xcarchive
-		xctool -workspace Breviar.xcworkspace -scheme "Breviar-$lang" -configuration "Breviar-$lang" clean
-		xctool -workspace Breviar.xcworkspace -scheme "Breviar-$lang" -configuration "Breviar-$lang" archive -archivePath build/Breviar-$lang
+		rm -rf build/Breviar-$lang-$version.xcarchive build/Breviar-$lang-$version.ipa
+		xcrun xcodebuild -workspace Breviar.xcworkspace -scheme "Breviar-$lang" -configuration "Release-$lang" clean
+		xcrun xcodebuild -workspace Breviar.xcworkspace -scheme "Breviar-$lang" -configuration "Release-$lang" archive -archivePath build/Breviar-$lang-$version
+		xcrun xcodebuild -exportArchive -exportOptionsPlist adhoc-export.plist -archivePath "build/Breviar-$lang-$version.xcarchive" -exportPath build
+		mv build/Breviar-$lang.ipa build/Breviar-$lang-$version.ipa
 	done
 )
