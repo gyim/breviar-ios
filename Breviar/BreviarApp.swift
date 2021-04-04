eae7a8026f55a1952e7f9b7a928096495d08b166
Akos Gyimesi <akos@gyim.hu>
2021-04-04 12:00:51 +0200

    Preload WKWebView on startup

--- Breviar/BreviarApp.swift
+++ Breviar/BreviarApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import WebKit
 
 @main
 struct BreviarApp: App {
     var body: some Scene {
         WindowGroup {
             MainScreen().environmentObject(BreviarModel.cgiModel())
+                .onAppear() {
+                    // Load an empty WKWebView so that it is faster to load on prayer screen
+                    _ = WKWebView()
+                }
         }
     }
 }
