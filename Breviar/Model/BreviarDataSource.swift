5a58488475d95590285ec8708452e909816ee58b
Akos Gyimesi <akos@gyim.hu>
2025-06-09 16:06:03 +0200

    Revert "Use the UI language on the About screen (no Latin)"
    This reverts commit f7a59783043fdba4140caa520a19dde520464cfd.

--- Breviar/Model/BreviarDataSource.swift
+++ Breviar/Model/BreviarDataSource.swift
@@ -315,14 +315,6 @@ class CGIDataSource : BreviarDataSource {
         }
     }
     
-    var cgiUILanguageCode: String {
-        if self.options?.calendar == "czop" {
-            return "c2"
-        } else {
-            return self.options?.uiLanguage.rawValue ?? ""
-        }
-    }
-    
     func getLiturgicalDay(day: Day, forceLocal: Bool, handler: @escaping (LiturgicalDay?, Error?) -> Void) {
         let args = [
             "qt": "pxml",
@@ -512,7 +504,7 @@ class CGIDataSource : BreviarDataSource {
             "qt": "pst",
             "st": "i",
             "p": "0",
-            "j": self.cgiUILanguageCode,
+            "j": self.cgiLanguageCode,
         ]
         self.cgiClient(forceLocal: true).makeRequest(args) { data, error in
             let response = String(data: data!, encoding: .utf8)
